57078f0c6d2f4bee852c32f350da2d440c05478a
SeanShih <seanshih@cw.com.tw>
2020-07-28 22:51:38 +0800

    modify demo database name and table name

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = 3000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
+// app.get("/items", (req, res) => res.send("Hello World!"));
+
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
