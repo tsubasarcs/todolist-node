4190d229eda244759cc4cf650a36941dc014f0c9
SeanShih <seanshih@cw.com.tw>
2020-07-29 22:21:12 +0800

    add cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
 
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
